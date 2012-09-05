c12ebd200aefe320daba77bac3fdb12796477eac
insup.choi <insup.choi@lge.com>
2012-09-04 23:53:48 -0700

    camera: increase q-factor to enhance image quality
    the q-factor value is changed to 90 from 85
    
    Change-Id: I847139cce956c3cb69bf9375135b34327e232cf0

--- camera/QCameraHWI_Parm.cpp
+++ camera/QCameraHWI_Parm.cpp
@@ -961,8 +961,8 @@ void QCameraHardwareInterface::initDefaultParameters()
     mParameters.set(QCameraParameters::KEY_SUPPORTED_PICTURE_FORMATS,
                     mPictureFormatValues);
 
-    mParameters.set(QCameraParameters::KEY_JPEG_QUALITY, "85"); // max quality
-    mJpegQuality = 85;
+    mParameters.set(QCameraParameters::KEY_JPEG_QUALITY, "90"); // max quality
+    mJpegQuality = 90;
     //Set Video Format
     mParameters.set(QCameraParameters::KEY_VIDEO_FRAME_FORMAT, "yuv420sp");
 
