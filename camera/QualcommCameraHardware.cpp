d2ca0ec79c4829437e06db2b5946621c548b93d4
Ankit Premrajka <ankitp@codeaurora.org>
2012-09-14 13:04:11 -0700

    camera: use new ION API.
    The ION API has changed to include a new filed called
    heap_mask in the ion_allocation_data structure. This new field
    now carried the heap bit mask that was previously carried
    by the flags field, which in turn will carry caching and other
    heap information.
    
    Change-Id: I28484a1ee9e4172c7abd208cdddad85069c3ea29
    
    Conflicts:
    
    	QCamera/stack/mm-camera-test/src/mm_qcamera_app.c

--- camera/QualcommCameraHardware.cpp
+++ camera/QualcommCameraHardware.cpp
@@ -3673,7 +3673,8 @@ int QualcommCameraHardware::allocate_ion_memory(int *main_ion_fd, struct ion_all
     /* to make it page size aligned */
     alloc->len = (alloc->len + 4095) & (~4095);
     alloc->align = 4096;
-    alloc->flags = (0x1 << ion_type | 0x1 << ION_IOMMU_HEAP_ID);
+    alloc->flags = 0;
+    alloc->heap_mask = (0x1 << ion_type | 0x1 << ION_IOMMU_HEAP_ID);
 
     rc = ioctl(*main_ion_fd, ION_IOC_ALLOC, alloc);
     if (rc < 0) {
