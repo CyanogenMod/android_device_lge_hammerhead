7ff2dd8af39035f2ccd02033a194b000788cf56e
insup.choi <insup.choi@lge.com>
2012-09-17 23:33:13 -0700

    camera: apply Fast Autofocus algorithm
    to speed-up the Auto-Focus performance.
    
    Change-Id: Id33ad29c583d6b6d79a865edadba729f0712f7f0

--- camera/mm-camera-interface/Android.mk
+++ camera/mm-camera-interface/Android.mk
@@ -14,6 +14,9 @@ MM_CAM_FILES:= \
         mm_omx_jpeg_encoder.c
 
 LOCAL_CFLAGS+= -D_ANDROID_
+ifeq ($(BOARD_USES_CAMERA_FAST_AUTOFOCUS),true)
+  LOCAL_CFLAGS += -DFAST_AF
+endif
 LOCAL_COPY_HEADERS_TO := mm-camera-interface
 LOCAL_COPY_HEADERS += mm_camera_interface2.h
 LOCAL_COPY_HEADERS += mm_omx_jpeg_encoder.h
