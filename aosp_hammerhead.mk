23dacf0c1a72ad7ecb957d36be5e5e6b1c5914a3
Jean-Baptiste Queru <jbq@google.com>
2013-04-03 17:18:53 -0700

    Fix aosp_hammerhead.
    Also use it by default in the lunch menu since the full_
    name has never been used in AOSP for that device.
    
    Change-Id: I0e6f4ffe420b1d9749da33089250b0d6c456f1d0

--- aosp_hammerhead.mk
+++ aosp_hammerhead.mk
@@ -13,6 +13,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
-$(call inherit-product, device/lge/mako/full_hammerhead.mk)
+$(call inherit-product, device/lge/hammerhead/full_hammerhead.mk)
 
 PRODUCT_NAME := aosp_hammerhead
