e747042bd5c8ec6fb4c5b4ef9251a1619c1ea7a1
Ajay Dudani <adudani@codeaurora.org>
2013-06-12 09:29:42 -0700

    Remove dependency on is-board-platform-in-list to determine target
    Change-Id: I58a1b1a35d7af865d2552e6c0a96fa22c0b44ed0

--- camera/Android.mk
+++ camera/Android.mk
@@ -1,9 +1,5 @@
 ifneq ($(strip $(USE_DEVICE_SPECIFIC_CAMERA)),true)
-MM_V4L2_DRIVER_LIST += msm8960
-MM_V4L2_DRIVER_LIST += msm8974
-MM_V4L2_DRIVER_LIST += msm8226
-
-ifeq ($(call is-board-platform-in-list,$(MM_V4L2_DRIVER_LIST)),true)
+ifneq ($(filter msm8960 msm8226 msm8974,$(TARGET_BOARD_PLATFORM)),)
   ifneq ($(USE_CAMERA_STUB),true)
     ifneq ($(BUILD_TINY_ANDROID),true)
       include $(call all-subdir-makefiles)
