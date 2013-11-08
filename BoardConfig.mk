0c2a442a8e40cf59a90d417b4313bdcdbb6c52d2
Danesh Mondegarian <daneshm90@gmail.com>
2013-11-07 18:39:52 -0800

    Update dtb inline kernel

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -28,6 +28,7 @@ BOARD_KERNEL_PAGESIZE := 2048
 
 BOARD_KERNEL_CMDLINE := console=ttyHSL0,115200,n8 androidboot.hardware=hammerhead user_debug=31 maxcpus=2 msm_watchdog_v2.enable=1
 BOARD_MKBOOTIMG_ARGS := --ramdisk_offset 0x02900000 --tags_offset 0x02700000
+BOARD_KERNEL_IMAGE_NAME := zImage-dtb
 
 # Shader cache config options
 # Maximum size of the  GLES Shaders that can be cached for reuse.
