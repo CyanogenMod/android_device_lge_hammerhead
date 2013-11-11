98f658cd433be396e649a4c19a0b83ea29cf037a
Scott <romracer@gmail.com>
2013-11-11 07:33:31 -0600

    Update extract-files.sh to allow pulling from disk (factory images)
    Change-Id: I4a3f1f491da8c964f78b145784368355fe5d578e

--- extract-files.sh
+++ extract-files.sh
@@ -1,7 +1,25 @@
 #!/bin/sh
 
+set -e
+
 VENDOR=lge
 DEVICE=hammerhead
+
+if [ $# -eq 0 ]; then
+  SRC=adb
+else
+  if [ $# -eq 1 ]; then
+    SRC=$1
+  else
+    echo "$0: bad number of arguments"
+    echo ""
+    echo "usage: $0 [PATH_TO_EXPANDED_ROM]"
+    echo ""
+    echo "If PATH_TO_EXPANDED_ROM is not specified, blobs will be extracted from"
+    echo "the device using adb pull."
+    exit 1
+  fi
+fi
 
 BASE=../../../vendor/$VENDOR/$DEVICE/proprietary
 rm -rf $BASE/*
@@ -11,5 +29,12 @@ for FILE in `cat proprietary-blobs.txt | grep -v ^# | grep -v ^$ | sed -e 's#^/s
     if [ ! -d $BASE/$DIR ]; then
         mkdir -p $BASE/$DIR
     fi
-    adb pull /system/$FILE $BASE/$FILE
+    if [ "$SRC" = "adb" ]; then
+      adb pull /system/$FILE $BASE/$FILE
+    else
+      cp $SRC/system/$FILE $BASE/$FILE
+    fi
+
 done
+
+./setup-makefiles.sh
