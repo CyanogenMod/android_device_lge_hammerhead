7c97d565f3546e1bb20d7b3d312930c6c9e68b7e
Scott <romracer@gmail.com>
2013-11-17 11:02:45 -0600

    hammerhead: Set compiler flags to neon-vfpv4
    Now that clang supports this fpu flag.
    
    Change-Id: Iac21617b0e1686722675f72dd7e0258506eea416

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -21,6 +21,11 @@ TARGET_ARCH := arm
 TARGET_ARCH_VARIANT := armv7-a-neon
 TARGET_CPU_VARIANT := krait
 
+# Flags for Krait CPU
+COMMON_GLOBAL_CFLAGS += -D__ARM_USE_PLD -D__ARM_CACHE_LINE_SIZE=64
+TARGET_GLOBAL_CFLAGS += -mfpu=neon-vfpv4 -mfloat-abi=softfp
+TARGET_GLOBAL_CPPFLAGS += -mfpu=neon-vfpv4 -mfloat-abi=softfp
+
 TARGET_NO_BOOTLOADER := true
 
 BOARD_KERNEL_BASE := 0x00000000
