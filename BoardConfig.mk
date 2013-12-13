628a76a441438a74cbc8e07711b7ad2d65e5e3c7
Steve Kondik <shade@chemlab.org>
2013-12-13 05:27:26 -0500

    hammerhead: Use cyanogenmod_hammerhead_defconfig
    Change-Id: Icd5b2bcce022ba6eb1680b2a75930979c91b698a

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -109,7 +109,7 @@ BOARD_SEPOLICY_DIRS += \
        device/lge/hammerhead/sepolicy
 
 # Define kernel config for inline building
-TARGET_KERNEL_CONFIG := hammerhead_defconfig
+TARGET_KERNEL_CONFIG := cyanogenmod_hammerhead_defconfig
 TARGET_KERNEL_SOURCE := kernel/lge/hammerhead
 
 # The list below is order dependent
