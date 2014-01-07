16a1c8e81a45b02ef4d468c72a7ccaec297d61ff
Steve Kondik <shade@chemlab.org>
2014-01-07 06:02:42 -0800

    hammerhead: Remove the maxcpus arg
     * This is fixed in the kernel now.
    
    Change-Id: Icfde43adfb9db4402232e7ef71e3e197dc193d43

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -27,7 +27,7 @@ BOARD_KERNEL_BASE := 0x00000000
 BOARD_KERNEL_PAGESIZE := 2048
 BOARD_KERNEL_SEPARATED_DT := true
 
-BOARD_KERNEL_CMDLINE := console=ttyHSL0,115200,n8 androidboot.hardware=hammerhead user_debug=31 maxcpus=2 msm_watchdog_v2.enable=1
+BOARD_KERNEL_CMDLINE := console=ttyHSL0,115200,n8 androidboot.hardware=hammerhead user_debug=31 msm_watchdog_v2.enable=1
 BOARD_MKBOOTIMG_ARGS := --ramdisk_offset 0x02900000 --tags_offset 0x02700000
 BOARD_KERNEL_IMAGE_NAME := zImage-dtb
 
