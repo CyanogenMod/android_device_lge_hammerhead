50e317be1853f7be3ee28b951696efdcda9ef513
Abhisek Devkota <ciwrl@cyanogenmod.com>
2014-02-20 17:28:36 -0800

    Add Bootloader Assert
    Per cyanogen, users on older bootloader/basebands are prone to
    adverse behavior in respect to recent commits for the device.
    
    Add assert to alert user to begin firmware update process manually.
    
    Change-Id: I332a5cd81a8ecf4a28fd278d448bb1a9a6c44158

--- releasetools.py
+++ releasetools.py
@@ -1,5 +1,41 @@
+# Copyright (C) 2014 The CyanogenMod Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 import common
 import struct
+import re
+import os
+
+TARGET_DIR = os.getenv('OUT')
+
+def FullOTA_Assertions(info):
+  AddBootloaderAssertion(info, info.input_zip)
+
+
+def IncrementalOTA_Assertions(info):
+  AddBootloaderAssertion(info, info.target_zip)
+
+
+def AddBootloaderAssertion(info, input_zip):
+  android_info = input_zip.read("OTA/android-info.txt")
+  m = re.search(r"require\s+version-bootloader\s*=\s*(\S+)", android_info)
+  if m:
+    bootloaders = m.group(1).split("|")
+    if "*" not in bootloaders:
+      info.script.AssertSomeBootloader(*bootloaders)
+    info.metadata["pre-bootloader"] = m.group(1)
+
 
 def FindRadio(zipfile):
   try:
