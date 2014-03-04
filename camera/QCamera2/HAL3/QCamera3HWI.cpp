691ea0660779d6ed4d95a4f6377d4ade8682a5ee
Shuzhen Wang <shuzhenw@codeaurora.org>
2014-03-04 01:47:40 -0800

    Camera3: Make maximum thumbnail size to be 4x3
    This aligns with most max resolution picture size
    aspect ratio. Also make the supported thumbnail
    sizes to be in increasing order per spec.
    
    Bug: 12101500
    Change-Id: I5ed791f08225605b3a538039a05576da458b9109

--- camera/QCamera2/HAL3/QCamera3HWI.cpp
+++ camera/QCamera2/HAL3/QCamera3HWI.cpp
@@ -136,8 +136,13 @@ const QCamera3HardwareInterface::QCameraMap QCamera3HardwareInterface::FACEDETEC
     { ANDROID_STATISTICS_FACE_DETECT_MODE_FULL,   CAM_FACE_DETECT_MODE_FULL    }
 };
 
-const int32_t available_thumbnail_sizes[] = {512, 288, 480, 288, 256, 154, 432, 288,
-                                             320, 240, 176, 144, 0, 0};
+const int32_t available_thumbnail_sizes[] = {0, 0,
+                                             176, 144,
+                                             320, 240,
+                                             432, 288,
+                                             480, 288,
+                                             512, 288,
+                                             512, 384};
 
 camera3_device_ops_t QCamera3HardwareInterface::mCameraOps = {
     initialize:                         QCamera3HardwareInterface::initialize,
