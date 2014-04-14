0bc21a3999f4ee1f95d8a5692a128a582996cda2
Steve Kondik <shade@chemlab.org>
2014-04-14 15:29:12 -0700

    hammerhead: Enable PCM offload
     * For offloading AV and streaming media.
    
    Change-Id: I19f67452fc830e626122320442b095b353feb822

--- device.mk
+++ device.mk
@@ -294,6 +294,8 @@ PRODUCT_PROPERTY_OVERRIDES += \
     af.resampler.quality=4 \
     audio.offload.buffer.size.kb=32 \
     audio.offload.gapless.enabled=false \
+    av.offload.enable=true \
+    av.streaming.offload.enable=true \
     av.offload.enable=true
 
 # Setup custom emergency number list based on the MCC. This is needed by RIL
