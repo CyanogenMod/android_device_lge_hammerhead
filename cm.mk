1649545f802a19d1d2db14bd70ba08b79aa4b23a
Tim Clark <tim.clark.82@gmail.com>
2014-06-06 10:03:28 -0700

    hammerhead: Update build fingerprint and description to KTU84M
    Change-Id: Ib7779d9d0f7ba9ba91f4560e1d3c3b673b543fb0

--- cm.mk
+++ cm.mk
@@ -18,7 +18,7 @@ PRODUCT_BRAND := google
 PRODUCT_MODEL := Nexus 5
 PRODUCT_MANUFACTURER := LGE
 
-PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=hammerhead BUILD_FINGERPRINT=google/hammerhead/hammerhead:4.4.2/KOT49H/937116:user/release-keys PRIVATE_BUILD_DESC="hammerhead-user 4.4.2 KOT49H 937116 release-keys"
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=hammerhead BUILD_FINGERPRINT=google/hammerhead/hammerhead:4.4.3/KTU84M/1158763:user/release-keys PRIVATE_BUILD_DESC="hammerhead-user 4.4.3 KTU84M 1158763 release-keys"
 
 # Enable Torch
 PRODUCT_PACKAGES += Torch
