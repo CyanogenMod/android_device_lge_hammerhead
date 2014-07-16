fc8dd104865fc3b8cf86098ae847ab4d5498fda9
Mike Lockwood <lockwood@google.com>
2014-07-16 11:24:42 -0700

    Add car_hammerhead build for Bluetooth automotive profile testing
    This is the same as aosp_hammerhead, but with the HFP client, A2DP sink
    and AVRCP controller profiles enabled.
    
    Bug: 16342993
    
    Change-Id: Ided5faad43bc9c8b689292bbb2de772b37f134b3

--- car_hammerhead.mk
+++ car_hammerhead.mk
@@ -1,5 +1,5 @@
 #
-# Copyright 2013 The Android Open Source Project
+# Copyright 2013 The Android Open-Source Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -14,8 +14,15 @@
 # limitations under the License.
 #
 
-PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/aosp_hammerhead.mk \
-    $(LOCAL_DIR)/full_hammerhead.mk \
-    $(LOCAL_DIR)/car_hammerhead.mk
+$(call inherit-product, device/lge/hammerhead/aosp_hammerhead.mk)
 
+# Add overlay to enable Bluetooth automotive profiles
+DEVICE_PACKAGE_OVERLAYS += \
+    device/lge/hammerhead/overlay_car
+
+PRODUCT_NAME := car_hammerhead
+PRODUCT_DEVICE := hammerhead
+PRODUCT_BRAND := Android
+PRODUCT_MODEL := Car Hammerhead
+PRODUCT_MANUFACTURER := LGE
+PRODUCT_RESTRICT_VENDOR_FILES := true
