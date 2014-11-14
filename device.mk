a0e107beaed80c179737e0c7bffe82d03797e059
myfluxi <linflux@arcor.de>
2014-11-14 16:29:30 +0100

    hammerhead: Audio update
    * Enable the compander for increased dynamic range on
      headphones and speaker output
    * Increase headphone gain
    * Build tinymix
    
    Change-Id: I78c317d91fe633907873b412d0f1e3e3b9da8ad1

--- device.mk
+++ device.mk
@@ -147,7 +147,8 @@ PRODUCT_PACKAGES += \
     audio.a2dp.default \
     audio.usb.default \
     audio.r_submix.default \
-    libaudio-resampler
+    libaudio-resampler \
+    tinymix
 
 # Audio effects
 PRODUCT_PACKAGES += \
