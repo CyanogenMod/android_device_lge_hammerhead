5ef6ba3fee63c6c1d72dcd623002a43a018c6572
Iliyan Malchev <malchev@google.com>
2014-12-17 10:16:37 -0800

    hammerhead: switch back to dlmalloc
    Experiment for a while with hammerhead switched back to dlmalloc (from
    jemalloc), on the theory that jemalloc was contributing to excessive
    page-allocator fragmentation. We will revisit this after the holidays once we
    have data from users.
    
    b/18069309 N5 running L can't run as many apps simultaneously as running K
    
    Change-Id: If69ddc33181e133045ffc1a6d98cdfc76078b388
    Signed-off-by: Iliyan Malchev <malchev@google.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -155,5 +155,7 @@ USE_DEVICE_SPECIFIC_CAMERA:= true
 # Enable Minikin text layout engine (will be the default soon)
 USE_MINIKIN := true
 
+MALLOC_IMPL := dlmalloc
+
 # Include an expanded selection of fonts
 EXTENDED_FONT_FOOTPRINT := true
