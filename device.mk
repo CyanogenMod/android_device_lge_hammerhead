ac39d6d93cf80ed081e4965730a9ab63ebf69adc
westcripp <korukaltan@gmail.com>
2014-12-18 20:06:53 +0200

    set dpi 400

--- device.mk
+++ device.mk
@@ -228,7 +228,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
     ro.opengles.version=196608
 
 PRODUCT_PROPERTY_OVERRIDES += \
-    ro.sf.lcd_density=480
+    ro.sf.lcd_density=400
 
 PRODUCT_PROPERTY_OVERRIDES += \
     persist.hwc.mdpcomp.enable=true
