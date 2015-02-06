bb50386c01f4aedc815691d223716a074d6bc96a
myfluxi <linflux@arcor.de>
2015-02-06 01:16:41 +0100

    hammerhead: Use halfres boot animation
    Change-Id: Iaaac09628e2e484a32b92a4099b540b55cf9a479

--- cm.mk
+++ cm.mk
@@ -1,6 +1,7 @@
 # Boot animation
 TARGET_SCREEN_HEIGHT := 1920
 TARGET_SCREEN_WIDTH := 1080
+TARGET_BOOTANIMATION_HALF_RES := true
 
 # Inherit some common CM stuff.
 $(call inherit-product, vendor/cm/config/common_full_phone.mk)
