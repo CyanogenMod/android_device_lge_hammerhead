e783eac1f3a810dbc505c708835ad1840a71e5e7
westcripp <korukaltan@gmail.com>
2015-02-14 00:07:45 +0200

    change dpi to 400
    Change-Id: If7362a5540e824625ea8fc5c6e9524916b2ff74a

--- device.mk
+++ device.mk
@@ -226,7 +226,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
     ro.opengles.version=196608
 
 PRODUCT_PROPERTY_OVERRIDES += \
-    ro.sf.lcd_density=430
+    ro.sf.lcd_density=4o0
 
 PRODUCT_PROPERTY_OVERRIDES += \
     persist.hwc.mdpcomp.enable=true
