d839c51631820259835df40715041f3023abef9e
westcripp <korukaltan@gmail.com>
2015-02-14 17:56:03 +0200

    return to stock DPI
    Change-Id: I574374d19d9052616e7a847f032a3c5f9dc8b0db

--- device.mk
+++ device.mk
@@ -226,7 +226,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
     ro.opengles.version=196608
 
 PRODUCT_PROPERTY_OVERRIDES += \
-    ro.sf.lcd_density=400
+    ro.sf.lcd_density=480
 
 PRODUCT_PROPERTY_OVERRIDES += \
     persist.hwc.mdpcomp.enable=true
