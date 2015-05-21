7e15ad95b745641845cdf5434b841fa2767e4249
myfluxi <linflux@arcor.de>
2015-05-21 21:35:17 +0200

    hammerhead: Update build fingerprint and description to LMY48B
    Change-Id: I16b3d8c402ffbb3c039640990cc00b865ad7fbce

--- cm.mk
+++ cm.mk
@@ -21,8 +21,8 @@ PRODUCT_MANUFACTURER := LGE
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME=hammerhead \
-    BUILD_FINGERPRINT=google/hammerhead/hammerhead:5.1/LMY47I/1767468:user/release-keys \
-    PRIVATE_BUILD_DESC="hammerhead-user 5.1 LMY47I 1767468 release-keys"
+    BUILD_FINGERPRINT=google/hammerhead/hammerhead:5.1.1/LMY48B/1863243:user/release-keys \
+    PRIVATE_BUILD_DESC="hammerhead-user 5.1.1 LMY48B 1863243 release-keys"
 
 # Enable Torch
 PRODUCT_PACKAGES += Torch
