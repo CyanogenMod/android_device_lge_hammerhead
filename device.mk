10d4173d7e49811f929f120096e219def00e0884
Thierry Strudel <tstrudel@google.com>
2015-07-16 18:30:40 -0700

    hammerhead: force dex2oat not to use swap file
    Bug: 20658562
    Change-Id: Ib8e47b3edd94b1977c294959cdde01a54b64f123
    Signed-off-by: Thierry Strudel <tstrudel@google.com>

--- device.mk
+++ device.mk
@@ -374,6 +374,9 @@ PRODUCT_PROPERTY_OVERRIDES += \
 PRODUCT_PROPERTY_OVERRIDES += \
     af.fast_track_multiplier=1
 
+PRODUCT_PROPERTY_OVERRIDES += \
+    dalvik.vm.dex2oat-swap=false
+
 # Modem debugger
 ifneq (,$(filter userdebug eng, $(TARGET_BUILD_VARIANT)))
 PRODUCT_PACKAGES += \
