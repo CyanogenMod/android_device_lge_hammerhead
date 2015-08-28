eac3ef869a8abce46ee6d336d5a73936ced83983
Marc vdW <linflux@arcor.de>
2015-08-28 03:11:25 -0700

    Revert "Revert: "hammerhead: prevent RIL from powering down SIM when modem is in low power mode""
    We need this prop, else users are bugged by a pin screen after
    toggling airplane mode.
    
    Addresses NIGHTLIES-1687
    
    This reverts commit 85efebfa21f522c4953ba14148420912f6ae8ce2.
    
    Change-Id: I8905349b1e64c8667d8c4a02cd2feec6d1fb2a57

--- device.mk
+++ device.mk
@@ -289,6 +289,10 @@ PRODUCT_PROPERTY_OVERRIDES += \
 PRODUCT_PROPERTY_OVERRIDES += \
     media.aac_51_output_enabled=true
 
+# Do not power down SIM card when modem is sent to Low Power Mode.
+PRODUCT_PROPERTY_OVERRIDES += \
+    persist.radio.apm_sim_not_pwdn=1
+
 # LTE, CDMA, GSM/WCDMA
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.ril.force_eri_from_xml=true \
