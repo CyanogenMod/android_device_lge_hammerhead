a93f5c8128c32a3cd9b9b1464f115911fa122725
myfluxi <linflux@arcor.de>
2015-12-08 20:02:45 +0100

    hammerhead: Update build fingerprint and description to MMB29K
    Change-Id: I94af0903ae3f54e09b326662db27febbc42980de

--- cm.mk
+++ cm.mk
@@ -21,5 +21,5 @@ PRODUCT_MANUFACTURER := LGE
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME=hammerhead \
-    BUILD_FINGERPRINT=google/hammerhead/hammerhead:6.0/MRA58N/2289998:user/release-keys \
-    PRIVATE_BUILD_DESC="hammerhead-user 6.0 MRA58N 2289998 release-keys"
+    BUILD_FINGERPRINT=google/hammerhead/hammerhead:6.0.1/MMB29K/2419427:user/release-keys \
+    PRIVATE_BUILD_DESC="hammerhead-user 6.0.1 MMB29K 2419427 release-keys"
