ec82c82fc99d5fe52fe939db64bccc295d248210
aexon <aexonpnz@gmail.com>
2016-02-02 20:43:17 +0300

    hammerhead: Update fingerprint for MMB29Q
    Change-Id: If549f34ab2023e67b8096d47ce7c666124f7ecb6

--- cm.mk
+++ cm.mk
@@ -21,5 +21,5 @@ PRODUCT_MANUFACTURER := LGE
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME=hammerhead \
-    BUILD_FINGERPRINT=google/hammerhead/hammerhead:6.0.1/MMB29S/2489379:user/release-keys \
-    PRIVATE_BUILD_DESC="hammerhead-user 6.0.1 MMB29S 2489379 release-keys"
+    BUILD_FINGERPRINT=google/hammerhead/hammerhead:6.0.1/MMB29Q/2480792:user/release-keys \
+    PRIVATE_BUILD_DESC="hammerhead-user 6.0.1 MMB29Q 2480792 release-keys"
