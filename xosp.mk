fbf2cfff07562b98f69369a40f1fb7b92d7a15ae
Shreesha Murthy <shreeshu10@gmail.com>
2016-02-25 16:44:06 +0530

    Add bootanim

--- xosp.mk
+++ xosp.mk
@@ -23,3 +23,5 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME=hammerhead \
     BUILD_FINGERPRINT=google/hammerhead/hammerhead:6.0.1/MMB29Q/2480792:user/release-keys \
     PRIVATE_BUILD_DESC="hammerhead-user 6.0.1 MMB29Q 2480792 release-keys"
+
+XOSP_BOOTANIMATOIN_NAME := 1080
