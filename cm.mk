f8dea4085af9af53200d741a10a9c984b2ea3398
Alexander Zandt <alexander.zandt@googlemail.com>
2016-04-17 23:16:03 +0200

    hammerhead: Update fingerprints to MOB30D
    Change-Id: I186665282a93df906aa59a4e5fb743aa5f551cdb

--- cm.mk
+++ cm.mk
@@ -21,5 +21,5 @@ PRODUCT_MANUFACTURER := LGE
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME=hammerhead \
-    BUILD_FINGERPRINT=google/hammerhead/hammerhead:6.0.1/MMB29X/2704508:user/release-keys \
-    PRIVATE_BUILD_DESC="hammerhead-user 6.0.1 MMB29X 2704508 release-keys"
+    BUILD_FINGERPRINT=google/hammerhead/hammerhead:6.0.1/MOB30D/2704746:user/release-keys \
+    PRIVATE_BUILD_DESC="hammerhead-user 6.0.1 MOB30D 2704746 release-keys"
