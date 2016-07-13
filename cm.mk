03ff1a9928010d3f4c962122f108980335176896
razorloves <razorloves@gmail.com>
2016-07-13 10:05:56 -0700

    hammerhead: update build fingerprint and description to MOB30P
    Change-Id: I77564e744c34afb0a6e8db534e0d9dac743a7f6d

--- cm.mk
+++ cm.mk
@@ -21,5 +21,5 @@ PRODUCT_MANUFACTURER := LGE
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME=hammerhead \
-    BUILD_FINGERPRINT=google/hammerhead/hammerhead:6.0.1/MOB30M/2862625:user/release-keys \
-    PRIVATE_BUILD_DESC="hammerhead-user 6.0.1 MOB30M 2862625 release-keys"
+    BUILD_FINGERPRINT=google/hammerhead/hammerhead:6.0.1/MOB30P/2960889:user/release-keys \
+    PRIVATE_BUILD_DESC="hammerhead-user 6.0.1 MOB30P 2960889 release-keys"
