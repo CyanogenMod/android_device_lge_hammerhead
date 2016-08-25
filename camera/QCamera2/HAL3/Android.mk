23bbad91fb23caa5ed7501deb0be0a6a176247fc
Chirayu Desai <chirayudesai1@gmail.com>
2016-08-25 20:37:06 +0530

    camera: Use project-path for HALs
    Change-Id: Ie259299709e0a31d52c3da367eb36911a3289100

--- camera/QCamera2/HAL3/Android.mk
+++ camera/QCamera2/HAL3/Android.mk
@@ -26,14 +26,14 @@ LOCAL_C_INCLUDES := \
         frameworks/native/include/media/openmax \
         frameworks/native/include \
         frameworks/av/include \
-        hardware/qcom/media/libstagefrighthw \
+        $(call project-path-for,qcom-media)/libstagefrighthw \
         system/media/camera/include \
         $(LOCAL_PATH)/../../mm-image-codec/qexif \
         $(LOCAL_PATH)/../../mm-image-codec/qomx_core \
         $(LOCAL_PATH)/../util
 
 LOCAL_C_INCLUDES += \
-        hardware/qcom/display/msm8974/libgralloc
+        $(call project-path-for,qcom-display)/msm8974/libgralloc
 
 LOCAL_C_INCLUDES += $(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ/usr/include
 LOCAL_ADDITIONAL_DEPENDENCIES += $(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ/usr
