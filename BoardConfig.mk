eb32a5990cee9d21c62741c007c2cb13743440d3
Chirayu Desai <chirayudesai1@gmail.com>
2016-08-27 17:07:14 +0530

    Use GCC 4.8 for kernel
    * 4.9 is the N / CM14 default, current kernel
      doesn't compile with it.
    
    TODO: Fix the kernel
    
    Change-Id: Ief997d1a64cbc0a0e7e6200989c29c9ca7197e39

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -28,6 +28,8 @@ BOARD_KERNEL_PAGESIZE := 2048
 BOARD_KERNEL_CMDLINE := console=ttyHSL0,115200,n8 androidboot.hardware=hammerhead user_debug=31 maxcpus=2 msm_watchdog_v2.enable=1 androidboot.bootdevice=msm_sdcc.1
 BOARD_MKBOOTIMG_ARGS := --ramdisk_offset 0x02900000 --tags_offset 0x02700000
 BOARD_KERNEL_IMAGE_NAME := zImage-dtb
+KERNEL_TOOLCHAIN := $(ANDROID_BUILD_TOP)/prebuilts/gcc/$(HOST_OS)-x86/arm/arm-eabi-4.8/bin
+KERNEL_TOOLCHAIN_PREFIX := arm-eabi-
 
 # Shader cache config options
 # Maximum size of the  GLES Shaders that can be cached for reuse.
