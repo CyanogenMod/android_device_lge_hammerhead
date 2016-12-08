1c8cd1999e040820ec8a1172d81d4c9d8cdd24d7
razorloves <razorloves@gmail.com>
2016-12-08 05:24:04 -0800

    hammerhead: update build fingerprint & description to M4B30Z
    Change-Id: Id21d89a10d6fbe922165cb89fbb2953f619a7f91

--- cm.mk
+++ cm.mk
@@ -18,5 +18,5 @@ PRODUCT_MANUFACTURER := LGE
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME=hammerhead \
-    BUILD_FINGERPRINT=google/hammerhead/hammerhead:6.0.1/M4B30X/3237893:user/release-keys \
-    PRIVATE_BUILD_DESC="hammerhead-user 6.0.1 M4B30X 3237893 release-keys"
+    BUILD_FINGERPRINT=google/hammerhead/hammerhead:6.0.1/M4B30Z/3437181:user/release-keys \
+    PRIVATE_BUILD_DESC="hammerhead-user 6.0.1 M4B30Z 3437181 release-keys"
